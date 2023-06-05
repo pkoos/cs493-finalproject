248cd3e16f3524c46a7f5025b53b223ed9129f91
Paul Koos <paully_k5298@yahoo.com>
2023-06-05 13:02:04 -0700

    initial commit.

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express, {Express, Request, Response} from 'express';
+import bodyParser from 'body-parser';
+
+const app: Express = express();
+const port = process.env.PORT ?? 8000;
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get('/test', (req: Request, res: Response) => {
+    console.log("test complete");
+    res.status(200).send();
+});
+
+app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}.`);
+});
