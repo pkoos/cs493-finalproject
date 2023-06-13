737a8748e6742d7b08ceb7641fffdc7b0d6fbe9c
Paul Koos <paully_k5298@yahoo.com>
2023-06-13 11:40:41 -0700

    add parent functions to race.ts.

--- src/models/race.ts
+++ src/models/race.ts
@@ -1,8 +1,16 @@
-export class Race {
+import { DatabaseModel } from "./database-model";
+
+export class Race extends DatabaseModel<Race> {
     id: number = -1;
     name: string = "";
     bonus_stats: number[] = [];
     description: string = "";
+
+    tableName: string = "Race";
+    public constructor(init?: Partial<Race>) {
+        super();
+        Object.assign(this, init);
+    }
 
     isValid(): boolean {
         const valid:boolean = this.name != undefined && this.name != "" &&
@@ -10,5 +18,29 @@ export class Race {
         this.description != undefined && this.description != "";
 
         return valid;
+    }
+
+    fromDatabase(data: any[]): Race {
+        const db_race: any = data[0];
+
+        const race: Race = new Race({
+            id: db_race.id,
+            name: db_race.name,
+            bonus_stats: db_race.bonus_stats,
+            description: db_race.description
+        });
+        return race;
+    }
+
+    insertParams(): any[] {
+        return [this.name, this.bonus_stats, this.description];
+    }
+
+    insertString(): string {
+        return `(name, bonus_stats, description) VALUES(?, ?, ?)`;
+    }
+
+    updateString(): string {
+        return `name=?, bonus_stats=?, description=?`;
     }
 }
