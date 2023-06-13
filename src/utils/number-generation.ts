09cbccacece657107e4f9f3f176a7c38ccaa132f
Doug Dennis <douglasdennisjr@gmail.com>
2023-06-13 00:01:51 -0700

    rearrange and add default to numDice

--- src/utils/number-generation.ts
+++ src/utils/number-generation.ts
@@ -1,4 +1,4 @@
-export function generate_dice_roll(numDice: number, numSides: number): number {
+export function generate_dice_roll(numSides: number, numDice: number = 1): number {
     let total: number = 0
     for (let i = 0; i < numDice; i++) {
         total += Math.floor(Math.random() * numSides + 1)
