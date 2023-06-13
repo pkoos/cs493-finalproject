7c7fcf0a501893c7e37b97e157a4099e4dba14aa
Paul Koos <paully_k5298@yahoo.com>
2023-06-13 11:15:07 -0700

    add functions required by parent to character.ts.

--- src/models/character.ts
+++ src/models/character.ts
@@ -1,6 +1,7 @@
+import { DatabaseModel } from './database-model';
 import { Equipment } from './equipment'
 
-export class Character {
+export class Character extends DatabaseModel<Character> {
     id: number = -1;
     owner_id: number = -1;
     name: string = "";
@@ -13,6 +14,12 @@ export class Character {
     alignment: string = "";
     description: string = "";
     inventory: Equipment[] = [];
+
+    tableName = "Player_Character";
+    public constructor(init?: Partial<Character>) {
+        super();
+        Object.assign(this, init);
+    }
 
     isValid(): boolean {
         const valid: boolean = 
@@ -28,5 +35,39 @@ export class Character {
             this.inventory != undefined && this.inventory.length != 0;
             
         return valid;
+    }
+    
+    fromDatabase(data: any[]): Character {
+        const db_data: any = data[0];
+        this.id = db_data.id;
+        this.owner_id = db_data.owner_id;
+        this.class_id = db_data.class_id;
+        this.race_id = db_data.race_id;
+        this.hitpoints = db_data.hitpoints;
+        this.raw_stats = db_data.raw_stats;
+        this.mod_stats = db_data.mod_stats;
+        this.background = db_data.background;
+        this.alignment = db_data.alignment;
+        this.description = db_data.description;
+        this.inventory = db_data.inventory;
+
+        return this;
+    }
+
+    insertParams(): any[] {
+        return [
+            this.owner_id, this.name, this.class_id, this.race_id, this.hitpoints, this.raw_stats, 
+            this.mod_stats, this.background, this.alignment, this.description
+        ];
+    }
+
+    insertString(): string {
+        return `(owner_id, class_id, race_id, hitpoints, raw_stats, mod_stats, background, alignment, inventory, description)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
+    }
+
+    updateString(): string {
+        return `owner_id=?, class_id=?, race_id=?, hitpoints=?, raw_stats=?,
+            mod_stats=?, background=?, alignment=?, description=?, inventory=?`
     }
 }
