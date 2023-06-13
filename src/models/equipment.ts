c327a97143db85aea2dc1e289d2a30a9985997fc
Paul Koos <paully_k5298@yahoo.com>
2023-06-13 11:36:21 -0700

    add parent functions to equipment.ts.

--- src/models/equipment.ts
+++ src/models/equipment.ts
@@ -1,9 +1,17 @@
-export class Equipment {
+import { DatabaseModel } from "./database-model";
+
+export class Equipment extends DatabaseModel<Equipment> {
     id: number = -1;
     name: string ="";
     equipment_type: number = -1;
     description: string = "";
     cost: number = -1;
+
+    tableName: string = "Equipment";
+    public constructor(init?: Partial<Equipment>) {
+        super();
+        Object.assign(this, init);
+    }
 
     isValid(): boolean {
         const valid: boolean = 
@@ -13,5 +21,31 @@ export class Equipment {
             this.cost != undefined && this.cost != -1;
             
         return valid;
+    }
+
+    fromDatabase(data: any[]): Equipment {
+        const db_equipment: any = data[0];
+
+        const equipment: Equipment = new Equipment({
+            id: db_equipment.id,
+            name: db_equipment.name,
+            equipment_type: db_equipment.equipment_type,
+            description: db_equipment.description,
+            cost: db_equipment.cost
+        });
+
+        return equipment;
+    }
+
+    insertParams(): any[] {
+        return [this.name, this.equipment_type, this.description, this.cost];
+    }
+
+    insertString(): string {
+        return `(name, equipment_type, description, cost) VALUES(?, ?, ?, ?)`;
+    }
+
+    updateString(): string {
+        return `name=?, equipment_type=?, description=?, cost=?`;
     }
 }
