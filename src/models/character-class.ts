591880cf7fd34d0fb54ceb20fc2898d718f56b48
Paul Koos <paully_k5298@yahoo.com>
2023-06-13 11:10:38 -0700

    add functions to chracter-class as required by parent.

--- src/models/character-class.ts
+++ src/models/character-class.ts
@@ -1,9 +1,17 @@
-export class CharacterClass {
+import { DatabaseModel } from "./database-model";
+
+export class CharacterClass extends DatabaseModel<CharacterClass> {
     id: number = -1;
     name: string = "";
     bonus_stats: number[] = [];
     description: string = "";
     hit_die: number = -1;
+
+    tableName = "Character_Class";
+    public constructor(init?: Partial<CharacterClass>) {
+        super();
+        Object.assign(this, init);
+    }
 
     isValid(): boolean {
         const valid: boolean = 
@@ -12,5 +20,37 @@ export class CharacterClass {
             this.description != undefined && this.description != "" &&
             this.hit_die != undefined && this.hit_die != -1;
         return valid;
+    }
+
+    fromDatabase(data: any[]): CharacterClass {
+        const db_class: any = data[0];
+        const charClass: CharacterClass = new CharacterClass({
+            id: db_class.id,
+            name: db_class.name,
+            bonus_stats: db_class.bonus_stats,
+            description: db_class.description,
+            hit_die: db_class.hit_die
+        });
+        
+        return charClass;
+    }
+
+    updateParams(): any[] {
+        const params: any[] = this.insertParams();
+        params.push(this.id);
+
+        return params;
+    }
+
+    insertParams(): any[] {
+        return [this.name, this.bonus_stats, this.description, this.hit_die];
+    }
+
+    insertString(): string {
+        return `(name, bonus_stats, description, hit_die) VALUES(?, ?, ?, ?)`;
+    }
+
+    updateString(): string {
+        return `name=?, email=?, password=?, type=?`;
     }
 }
