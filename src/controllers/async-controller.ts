c076ef6f3f7f181b37429b7740a2d4c39e4a0676
Doug Dennis <douglasdennisjr@gmail.com>
2023-06-14 02:43:22 -0700

    added chatgpt async function

--- src/controllers/async-controller.ts
+++ src/controllers/async-controller.ts
@@ -1,13 +1,27 @@
+import { Request, Response } from 'express';
 import amqp from 'amqplib';
 import Jimp from 'jimp';
 
 import { CharacterImage } from '../models/character-image';
 
+import { generate_dice_roll } from '../utils/number-generation';
+import { successResponse } from '../utils/responses-helper';
+
+import { Configuration, OpenAIApi } from "openai";
+
+const openai_configuration = new Configuration({
+    organization: process.env.OPENAI_ORG,
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(openai_configuration);
+
 const rabbitmqHost: string = process.env.RABBITMQ_HOST ?? "localhost";
 const rabbitmqUrl: string = `amqp://${rabbitmqHost}`;
 var connection: amqp.Connection;
 var channel: amqp.Channel;
-const queues: string[] = ['testQueue', 'thumbnailQueue'];
+
+const queues: string[] = ['testQueue', 'thumbnailQueue', 'characterDescriptionQueue'];
 
 async function generateThumbnail(message: amqp.ConsumeMessage | null): Promise<void> {
     if (message === null) {
@@ -36,6 +50,32 @@ async function generateThumbnail(message: amqp.ConsumeMessage | null): Promise<v
     console.log("Finished generating thumbnail for image " + image_id);
 }
 
+async function generateCharacterDescription(message: amqp.ConsumeMessage | null): Promise<void> {
+    if (message === null) {
+        return;
+    }
+
+    channel.ack(message);
+
+    const pc_id: number = parseInt(message.content.toString());
+
+    const str: number = generate_dice_roll(3, 6);
+    const dex: number = generate_dice_roll(3, 6);
+    const con: number = generate_dice_roll(3, 6);
+    const int: number = generate_dice_roll(3, 6);
+    const wis: number = generate_dice_roll(3, 6);
+    const cha: number = generate_dice_roll(3, 6);
+
+    const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [
+            {"role": "system", "content": "You are a fantastic bard who produces epic stories of heroes."},
+            {role: "user", content: `Generate a short character description of a barbarian with a ${str} strength, ${dex} dexterity, ${con} constitution, ${int} intelligence, ${wis} wisdom, and ${cha} charisma.`}
+        ],
+    });
+    console.log(completion.data.choices[0].message?.content);
+}
+
 export async function initializeAsyncController() {
     connection = await amqp.connect(rabbitmqUrl);
     channel = await connection.createChannel();
@@ -43,6 +83,7 @@ export async function initializeAsyncController() {
 
     channel.consume('testQueue', (message) => { console.log(message?.content.toString()); }, { noAck: true });
     channel.consume('thumbnailQueue', generateThumbnail, { noAck: false });
+    channel.consume('characterDescriptionQueue', generateCharacterDescription, { noAck: false });
 }
 
 export async function requestTest(message: string) {
@@ -51,4 +92,10 @@ export async function requestTest(message: string) {
 
 export async function requestImageThumbnail(image_id: number): Promise<void> {
     channel.sendToQueue('thumbnailQueue', Buffer.from(image_id.toString()));
+}
+
+export function requestCharacterDescription(req: Request, res: Response) {
+    const pc_id: number = parseInt(req.params.pc_id);
+    channel.sendToQueue('characterDescriptionQueue', Buffer.from(pc_id.toString()));
+    successResponse(res, {pc_id: pc_id});
 }
