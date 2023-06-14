7453e4ed50de42ac6462c76209eb6f156474c9a8
Doug Dennis <douglasdennisjr@gmail.com>
2023-06-14 14:56:36 -0700

    fix up character image stuff

--- src/controllers/character-image-controller.ts
+++ src/controllers/character-image-controller.ts
@@ -2,7 +2,8 @@ import { Express, Request, Response } from 'express';
 
 import { CharacterImage } from '../models/character-image';
 import { requestImageThumbnail } from './async-controller';
-import { errorServer, errorInvalidBody, successResponse } from '../utils/responses-helper';
+import { errorServer, errorInvalidBody, successResponse, errorUnauthorizedUser } from '../utils/responses-helper';
+import { Character } from '../models/character';
 
 export async function addCharacterImage(req: Request, res: Response): Promise<void> {
     if (req.file === undefined) {
@@ -15,11 +16,23 @@ export async function addCharacterImage(req: Request, res: Response): Promise<vo
         return;
     }
 
+    const playerCharacter: Character | undefined = await new Character().findById(req.body["pc_id"]);
+    if (playerCharacter === undefined || !playerCharacter.isValid()) {
+        console.log(`Invalid character id for addCharacterImage: ${req.body["pc_id"]}`);
+        errorInvalidBody(res);
+        return;
+    }
+
+    if (playerCharacter.owner_id !== req.loggedInID) {
+        errorUnauthorizedUser(res, "add character image to a character they do not own.");
+        return;
+    }
+
     // create an image object
     const file: Express.Multer.File = req.file;
     try {
         const image: CharacterImage = new CharacterImage({
-            owner_id: 0,
+            owner_id: req.loggedInID,
             pc_id: req.body["pc_id"],
             image_name: file.originalname,
             content_type: file.mimetype,
@@ -59,8 +72,8 @@ export async function getCharacterImage(req: Request, res: Response): Promise<vo
 
 export async function getCharacterImageThumbnail(req: Request, res: Response): Promise<void> {
     const image_id: number = parseInt(req.params.id);
-    const image: CharacterImage = await new CharacterImage().findById(image_id);
-    if (!image.isValid()) {
+    const image: CharacterImage | undefined = await new CharacterImage().findById(image_id);
+    if (image === undefined || !image.isValid()) {
         console.log(image);
         errorInvalidBody(res);
         return;
