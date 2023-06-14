b12526f57e12d898cc44419cf25b1ed8b21613e0
Doug Dennis <douglasdennisjr@gmail.com>
2023-06-14 16:40:39 -0700

    modified character to match db

--- src/models/character.ts
+++ src/models/character.ts
@@ -5,14 +5,11 @@ export class Character extends DatabaseModel<Character> {
     owner_id: number = -1;
     class_id: number = -1;
     race_id: number = -1;
-    stats_id: number = -1;
     name: string = "";
     hitpoints: number = -1;
+    stats_id: number = -1;
     background: string = "";
     alignment: string = "";
-    description: string = "";
-    inventory_id: number = -1;
-    image_id: number = -1;
 
     tableName = "Player_Character";
     public constructor(init?: Partial<Character>) {
@@ -21,7 +18,7 @@ export class Character extends DatabaseModel<Character> {
     }
 
     isValid(): boolean {
-        const valid: boolean = 
+        const valid: boolean =
             this.owner_id != undefined && this.owner_id != -1 &&
             this.class_id != undefined && this.class_id != -1 &&
             this.race_id != undefined && this.race_id != -1 &&
@@ -29,14 +26,10 @@ export class Character extends DatabaseModel<Character> {
             this.name != undefined && this.name != "" &&
             this.hitpoints != undefined && this.hitpoints != -1 &&
             this.background != undefined && this.background != "" &&
-            this.alignment != undefined && this.alignment != "" &&
-            this.description != undefined && this.description != "" &&
-            this.inventory_id != undefined && this.inventory_id != -1 &&
-            this.image_id != undefined && this.image_id != -1;
-            
+            this.alignment != undefined && this.alignment != "";
         return valid;
     }
-    
+
     fromDatabase(data: any[]): Character {
         const db_data: any = data[0];
         this.id = db_data.id;
@@ -48,26 +41,24 @@ export class Character extends DatabaseModel<Character> {
         this.hitpoints = db_data.hitpoints;
         this.background = db_data.background;
         this.alignment = db_data.alignment;
-        this.description = db_data.description;
-        this.inventory_id = db_data.inventory_id;
-        this.image_id = db_data.image_id;
 
         return this;
     }
 
     insertParams(): any[] {
         return [
-            this.owner_id, this.class_id, this.race_id, this.stats_id, this.name, this.hitpoints,  
-            this.background, this.alignment, this.description, this.inventory_id, this.image_id
+            this.owner_id, this.name, this.class_id, this.race_id, this.hitpoints, this.stats_id,
+            this.background, this.alignment
         ];
     }
 
     insertString(): string {
-        return `(owner_id, class_id, race_id, stats_id, name, hitpoints, background, alignment, description, inventory_id, image_id)
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+        return `(owner_id, name, class_id, race_id, hitpoints, stats_id, background, alignment)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?)`
     }
 
     updateString(): string {
-        return `owner_id=?, class_id=?, race_id=?, stats_id=?, name=?, hitpoints=?, background=?, alignment=?, description=?, inventory_id=?, image_id=?`
+        return `owner_id=?, name=?, class_id=?, race_id=?, hitpoints=?, stats_id=?,
+            background=?, alignment=?`
     }
 }
